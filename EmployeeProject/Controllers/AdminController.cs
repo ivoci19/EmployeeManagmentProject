c1abefe5717f4ede9cadae18faaf642ab85e92ba
irklidavoci <irklidavoci9@gmail.com>
2022-08-18 10:56:08 +0200

    Updating Code

--- EmployeeProject/Controllers/AdminController.cs
+++ EmployeeProject/Controllers/AdminController.cs
@@ -14,7 +14,7 @@ namespace EmployeeProject.Controllers
     [Route("api/[controller]")]
     [ApiController]
     [Authorize(Roles = "Administrator")]
-    //Ka te beje kjo? qe e kisha si koment
+
     public class AdminController : ControllerBase
     {
         private readonly IUserServices _userServices;
@@ -121,93 +121,7 @@ namespace EmployeeProject.Controllers
         }
 
 
-        [HttpGet("GetAllProjects")]
-        public IActionResult GetAllProjects()
-        {
-            try
-            {
-                var projects = _projectServices.GetAllProjects();
-                return Ok(projects);
-            }
-            catch (Exception e)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-        }
-
-
-        [HttpGet("GetProject{id:int}")]
-        public IActionResult GetProject(int id)
-        {
-            try
-            {
-                var project = _projectServices.GetProjectById(id);
-                return Ok(project);
-            }
-            catch (Exception e)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-        }
-
-        [HttpPost("CreateProject")]
-        public ActionResult CreateProject(ProjectEditViewModel project)
-        {
-            try
-            {
-                if (project == null)
-                    return BadRequest();
-
-                var createdProject = _projectServices.CreateProject(project);
-
-                return CreatedAtAction(nameof(GetProject), new { id = createdProject.Id }, createdProject);
-            }
-            catch (Exception e)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-        }
-
-
-        [HttpPut("UpdateProject{id:int}")]
-        public IActionResult UpdateProject(int id, ProjectEditViewModel project)
-        {
-            try
-            {
-                var projectToUpdate = _projectServices.GetProjectById(id);
-
-                if (projectToUpdate == null)
-                    return NotFound("Project with Id = " + id.ToString() + " not found");
-
-                if (project == null)
-                    return BadRequest();
-
-                return Ok(_projectServices.UpdateProject(project, id));
-            }
-            catch (Exception e)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-        }
-
-        [HttpDelete("DeleteProject{id:int}")]
-        public IActionResult DeleteProject(int id)
-        {
-            try
-            {
-                var project = _projectServices.GetProjectById(id);
-                if (project == null)
-                    return NotFound("Project with Id = " + id.ToString() + " not found");
-
-                _projectServices.DeleteProject(id);
-                return Ok(project);
-            }
-            catch (Exception e)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-        }
-
+      
         [HttpGet("GetAllTasks")]
         public IActionResult GetAllTasks()
         {
---
+++ EmployeeProject/Controllers/ProjectsController.cs
@@ -0,0 +1,111 @@
+﻿using EmployeeServices.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedModels.ViewModels;
+using System;
+
+namespace EmployeeProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly IUserServices _userServices;
+        private readonly IProjectServices _projectServices;
+        private readonly ITaskServices _taskServices;
+        public ProjectsController(IUserServices userServices, IProjectServices projectServices, ITaskServices taskServices)
+        {
+            _userServices = userServices;
+            _projectServices = projectServices;
+            _taskServices = taskServices;
+        }
+
+        [HttpGet("GetAllProjects")]
+        public IActionResult GetAllProjects()
+        {
+            try
+            {
+                var projects = _projectServices.GetAllProjects();
+                return Ok(projects);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+
+        [HttpGet("GetProject{id:int}")]
+        public IActionResult GetProject(int id)
+        {
+            try
+            {
+                var project = _projectServices.GetProjectById(id);
+                return Ok(project);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPost("CreateProject")]
+        public ActionResult CreateProject(ProjectEditViewModel project)
+        {
+            try
+            {
+                if (project == null)
+                    return BadRequest();
+
+                var createdProject = _projectServices.CreateProject(project);
+
+                return CreatedAtAction(nameof(GetProject), new { id = createdProject.Id }, createdProject);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+
+        [HttpPut("UpdateProject{id:int}")]
+        public IActionResult UpdateProject(int id, ProjectEditViewModel project)
+        {
+            try
+            {
+                var projectToUpdate = _projectServices.GetProjectById(id);
+
+                if (projectToUpdate == null)
+                    return NotFound("Project with Id = " + id.ToString() + " not found");
+
+                if (project == null)
+                    return BadRequest();
+
+                return Ok(_projectServices.UpdateProject(project, id));
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpDelete("DeleteProject{id:int}")]
+        public IActionResult DeleteProject(int id)
+        {
+            try
+            {
+                var project = _projectServices.GetProjectById(id);
+                if (project == null)
+                    return NotFound("Project with Id = " + id.ToString() + " not found");
+
+                _projectServices.DeleteProject(id);
+                return Ok(project);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+    }
+}
